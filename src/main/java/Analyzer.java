aa5778bb1cb854795a96aa7e75122b690f75e130
Nikolai Pavlov <nik220822@rambler.ru>
2023-05-18 12:08:25 +0300

    fixed

--- src/main/java/Analyzer.java
+++ src/main/java/Analyzer.java
@@ -16,88 +16,47 @@ public class Analyzer {
     private static final BlockingQueue<String> cArrayBlockingQueue = new ArrayBlockingQueue<>(queueCapacity);
 
     public static void main(String[] a) throws InterruptedException {
-        Thread stringFiller = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (textCount < numberOfTexts) {
-                    String string = generateText(letters, length);
-                    textCount = textCount + 1;
-                    try {
-                        aArrayBlockingQueue.put(string);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    try {
-                        bArrayBlockingQueue.put(string);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    try {
-                        cArrayBlockingQueue.put(string);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
+        Thread stringFiller = new Thread(() -> {
+            while (textCount < numberOfTexts) {
+                String string = generateText(letters, length);
+                textCount = textCount + 1;
+                try {
+                    aArrayBlockingQueue.put(string);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    bArrayBlockingQueue.put(string);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    cArrayBlockingQueue.put(string);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
         });
         stringFiller.start();
-        Thread aCounter = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int number;
-                while (true) {
-                    try {
-                        String string = aArrayBlockingQueue.take();
-                        number = Math.toIntExact(string.chars().filter(ch -> ch == 'a').count());
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    if (number > aNumber) {
-                        aNumber = number;
-                        System.out.println("Максимальное количество букв \"a\" в одной строке: " + aNumber);
-                    }
-                }
-            }
+        Thread aCounter = new Thread(() -> {
+            processStrings('a', aNumber, (ArrayBlockingQueue) aArrayBlockingQueue);
         });
         aCounter.start();
-        Thread bCounter = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int number;
-                while (true) {
-                    try {
-                        String string = bArrayBlockingQueue.take();
-                        number = Math.toIntExact(string.chars().filter(ch -> ch == 'b').count());
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    if (number > bNumber) {
-                        bNumber = number;
-                        System.out.println("Максимальное количество букв \"b\" в одной строке: " + bNumber);
-                    }
-                }
-            }
+        Thread bCounter = new Thread(() -> {
+            processStrings('b', bNumber, (ArrayBlockingQueue) bArrayBlockingQueue);
         });
         bCounter.start();
-        Thread cCounter = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int number;
-                while (true) {
-                    try {
-                        String string = cArrayBlockingQueue.take();
-                        number = Math.toIntExact(string.chars().filter(ch -> ch == 'c').count());
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    if (number > cNumber) {
-                        cNumber = number;
-                        System.out.println("Максимальное количество букв \"c\" в одной строке: " + cNumber);
-                    }
-                }
-            }
+        Thread cCounter = new Thread(() -> {
+            processStrings('c', cNumber, (ArrayBlockingQueue) cArrayBlockingQueue);
         });
         cCounter.start();
+        stringFiller.join();
+        aCounter.join();
+        bCounter.join();
+        cCounter.join();
+        System.out.println("Максимальное количество букв \"a\" в одной строке: " + aNumber);
+        System.out.println("Максимальное количество букв \"b\" в одной строке: " + bNumber);
+        System.out.println("Максимальное количество букв \"c\" в одной строке: " + cNumber);
     }
 
     public static String generateText(String letters, int length) {
@@ -107,5 +66,30 @@ public class Analyzer {
             text.append(letters.charAt(random.nextInt(letters.length())));
         }
         return text.toString();
+    }
+
+    private static void processStrings(char chr, int xNumber, ArrayBlockingQueue arrayBlockingQueue) {
+        int number;
+        while (textCount < numberOfTexts) {
+            try {
+                String string = (String) arrayBlockingQueue.take();
+                number = Math.toIntExact(string.chars().filter(ch -> ch == chr).count());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (number > xNumber) {
+                switch (chr) {
+                    case ('a'):
+                        aNumber = number;
+                        break;
+                    case ('b'):
+                        bNumber = number;
+                        break;
+                    case ('c'):
+                        cNumber = number;
+                        break;
+                }
+            }
+        }
     }
 }
